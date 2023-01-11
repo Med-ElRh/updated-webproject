ee1e6ab203457e4ee1d4cd2d3d2e22c8e3e7229b
Med-ElRh <119575188+Med-ElRh@users.noreply.github.com>
2023-01-11 02:57:10 +0100

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,33 @@
+$('#productCarousel').carousel({
+  interval: 5000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
+$('.carousel').bcSwipe({ threshold: 15 });
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
